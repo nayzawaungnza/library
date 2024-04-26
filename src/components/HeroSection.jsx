99b29b05c674d488f9e028270f6fce59503db2a1
nayzawaungnza <nayzawaung750@gmail.com>
2024-04-26 15:21:12 +0630

    Ep-56-Home-Page-Designs

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 h-48 flex items-center justify-center">
+    <div className="space-y-2">
+    <h1 className="text-center text-white text-3xl"> Welcome from my Library</h1>
+    <p className="text-gray-300 text-sm">A place where you can store and manage your books.</p>
+    </div>
+   </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import HeroSection from "../components/HeroSection"
+import lifeofthecandle from "../assets/life-of-the-candle.jpg"
 
 function Home() {
 
   return (
     <>
-     <h1 className='text-blue-500'>Hello World</h1>
+      <HeroSection />
+     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3">
+      {[1,2,3,4,5].map(()=>(
+        <div className=" p-4 border border-1">
+          <img src={lifeofthecandle} alt="" />
+          <div className="text-center space-y-2 mt-3">
+            <h1 className="">Book Title</h1>
+            <p className="">Description</p>
+            <div className="flex flex-wrap">
+              {['travels','knowledge', 'internet', 'programming'].map(genre =>(
+                <span className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500">{genre}</span>
+              ))}
+            </div>
+          </div>
+          
+        </div>
+      ))}
+     </div>
     </>
   )
 }
