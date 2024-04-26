47d169952164ecd6bd766ae3a9f68d841a006f6e
nayzawaungnza <nayzawaung750@gmail.com>
2024-04-26 15:34:40 +0630

    Ep-57-How-to-add-page-transition-101

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
-
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import './styles.css'
 export default function Layout() {
+    const location = useLocation();
+
   return (
     <div>
      <Navbar/>
-      <div className='max-w-6xl mx-auto p-3'>
-      <Outlet/>
-      </div>
+      <SwitchTransition>
+        <CSSTransition timeout={2000} classNames='fade' key={location.pathname}>
+            <div className='max-w-6xl mx-auto p-3'>
+                <Outlet/>
+            </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   )
 }
